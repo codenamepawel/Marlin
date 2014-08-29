5958d09c9f516070f8e02f19469f8c0c52dcf928
codenamepawel <codenamepawel@tlen.pl>
2014-08-29 22:12:32 +0200

    Update language.h
    Added option difrent console

--- Marlin/language.h
+++ Marlin/language.h
@@ -67,7 +67,81 @@
 // Common serial messages
 #define MSG_MARLIN "Marlin"
 
+#ifdef CONSOLE_LANGUAGE_EN
+	#define MSG_Enqueing "enqueing \""
+	#define MSG_POWERUP "PowerUp"
+	#define MSG_EXTERNAL_RESET " External Reset"
+	#define MSG_BROWNOUT_RESET " Brown out Reset"
+	#define MSG_WATCHDOG_RESET " Watchdog Reset"
+	#define MSG_SOFTWARE_RESET " Software Reset"
+	#define MSG_AUTHOR " | Author: "
+	#define MSG_CONFIGURATION_VER " Last Updated: "
+	#define MSG_FREE_MEMORY " Free Memory: "
+	#define MSG_PLANNER_BUFFER_BYTES "  PlannerBufferBytes: "
+	#define MSG_OK "ok"
+	#define MSG_FILE_SAVED "Done saving file."
+	#define MSG_ERR_LINE_NO "Line Number is not Last Line Number+1, Last Line: "
+	#define MSG_ERR_CHECKSUM_MISMATCH "checksum mismatch, Last Line: "
+	#define MSG_ERR_NO_CHECKSUM "No Checksum with line number, Last Line: "
+	#define MSG_ERR_NO_LINENUMBER_WITH_CHECKSUM "No Line Number with checksum, Last Line: "
+	#define MSG_FILE_PRINTED "Done printing file"
+	#define MSG_BEGIN_FILE_LIST "Begin file list"
+	#define MSG_END_FILE_LIST "End file list"
+	#define MSG_M104_INVALID_EXTRUDER "M104 Invalid extruder "
+	#define MSG_M105_INVALID_EXTRUDER "M105 Invalid extruder "
+	#define MSG_M200_INVALID_EXTRUDER "M200 Invalid extruder "
+	#define MSG_M218_INVALID_EXTRUDER "M218 Invalid extruder "
+	#define MSG_M221_INVALID_EXTRUDER "M221 Invalid extruder "
+	#define MSG_ERR_NO_THERMISTORS "No thermistors - no temperature"
+	#define MSG_M109_INVALID_EXTRUDER "M109 Invalid extruder "
+	#define MSG_HEATING "Heating..."
+	#define MSG_HEATING_COMPLETE "Heating done."
+	#define MSG_BED_HEATING "Bed Heating."
+	#define MSG_BED_DONE "Bed done."
+	#define MSG_M115_REPORT "FIRMWARE_NAME:Marlin V1; Sprinter/grbl mashup for gen6 FIRMWARE_URL:" FIRMWARE_URL " PROTOCOL_VERSION:" PROTOCOL_VERSION " MACHINE_TYPE:" MACHINE_NAME " EXTRUDER_COUNT:" STRINGIFY(EXTRUDERS) " UUID:" MACHINE_UUID "\n"
+	#define MSG_COUNT_X " Count X: "
+	#define MSG_ERR_KILLED "Printer halted. kill() called!"
+	#define MSG_ERR_STOPPED "Printer stopped due to errors. Fix the error and use M999 to restart. (Temperature is reset. Set it after restarting)"
+	#define MSG_RESEND "Resend: "
+	#define MSG_UNKNOWN_COMMAND "Unknown command: \""
+	#define MSG_ACTIVE_EXTRUDER "Active Extruder: "
+	#define MSG_INVALID_EXTRUDER "Invalid extruder"
+	#define MSG_X_MIN "x_min: "
+	#define MSG_X_MAX "x_max: "
+	#define MSG_Y_MIN "y_min: "
+	#define MSG_Y_MAX "y_max: "
+	#define MSG_Z_MIN "z_min: "
+	#define MSG_Z_MAX "z_max: "
+	#define MSG_M119_REPORT "Reporting endstop status"
+	#define MSG_ENDSTOP_HIT "TRIGGERED"
+	#define MSG_ENDSTOP_OPEN "open"
+	#define MSG_HOTEND_OFFSET "Hotend offsets:"
 
+	#define MSG_SD_CANT_OPEN_SUBDIR "Cannot open subdir"
+	#define MSG_SD_INIT_FAIL "SD init fail"
+	#define MSG_SD_VOL_INIT_FAIL "volume.init failed"
+	#define MSG_SD_OPENROOT_FAIL "openRoot failed"
+	#define MSG_SD_CARD_OK "SD card ok"
+	#define MSG_SD_WORKDIR_FAIL "workDir open failed"
+	#define MSG_SD_OPEN_FILE_FAIL "open failed, File: "
+	#define MSG_SD_FILE_OPENED "File opened: "
+	#define MSG_SD_SIZE " Size: "
+	#define MSG_SD_FILE_SELECTED "File selected"
+	#define MSG_SD_WRITE_TO_FILE "Writing to file: "
+	#define MSG_SD_PRINTING_BYTE "SD printing byte "
+	#define MSG_SD_NOT_PRINTING "Not SD printing"
+	#define MSG_SD_ERR_WRITE_TO_FILE "error writing to file"
+	#define MSG_SD_CANT_ENTER_SUBDIR "Cannot enter subdir: "
+
+	#define MSG_STEPPER_TOO_HIGH "Steprate too high: "
+	#define MSG_ENDSTOPS_HIT "endstops hit: "
+	#define MSG_ERR_COLD_EXTRUDE_STOP " cold extrusion prevented"
+	#define MSG_ERR_LONG_EXTRUDE_STOP " too long extrusion prevented"
+	#define MSG_BABYSTEPPING_X "Babystepping X"
+	#define MSG_BABYSTEPPING_Y "Babystepping Y"
+	#define MSG_BABYSTEPPING_Z "Babystepping Z"
+	#define MSG_SERIAL_ERROR_MENU_STRUCTURE "Error in menu structure"
+#endif
 
 #if LANGUAGE_CHOICE == 1
 
@@ -394,81 +468,7 @@
 	#define MSG_CONTRAST "Kontrast"
 
 // Serial Console Messages
-#ifdef CONSOLE_LANGUAGE_EN
-	#define MSG_Enqueing "enqueing \""
-	#define MSG_POWERUP "PowerUp"
-	#define MSG_EXTERNAL_RESET " External Reset"
-	#define MSG_BROWNOUT_RESET " Brown out Reset"
-	#define MSG_WATCHDOG_RESET " Watchdog Reset"
-	#define MSG_SOFTWARE_RESET " Software Reset"
-	#define MSG_AUTHOR " | Author: "
-	#define MSG_CONFIGURATION_VER " Last Updated: "
-	#define MSG_FREE_MEMORY " Free Memory: "
-	#define MSG_PLANNER_BUFFER_BYTES "  PlannerBufferBytes: "
-	#define MSG_OK "ok"
-	#define MSG_FILE_SAVED "Done saving file."
-	#define MSG_ERR_LINE_NO "Line Number is not Last Line Number+1, Last Line: "
-	#define MSG_ERR_CHECKSUM_MISMATCH "checksum mismatch, Last Line: "
-	#define MSG_ERR_NO_CHECKSUM "No Checksum with line number, Last Line: "
-	#define MSG_ERR_NO_LINENUMBER_WITH_CHECKSUM "No Line Number with checksum, Last Line: "
-	#define MSG_FILE_PRINTED "Done printing file"
-	#define MSG_BEGIN_FILE_LIST "Begin file list"
-	#define MSG_END_FILE_LIST "End file list"
-	#define MSG_M104_INVALID_EXTRUDER "M104 Invalid extruder "
-	#define MSG_M105_INVALID_EXTRUDER "M105 Invalid extruder "
-	#define MSG_M200_INVALID_EXTRUDER "M200 Invalid extruder "
-	#define MSG_M218_INVALID_EXTRUDER "M218 Invalid extruder "
-	#define MSG_M221_INVALID_EXTRUDER "M221 Invalid extruder "
-	#define MSG_ERR_NO_THERMISTORS "No thermistors - no temperature"
-	#define MSG_M109_INVALID_EXTRUDER "M109 Invalid extruder "
-	#define MSG_HEATING "Heating..."
-	#define MSG_HEATING_COMPLETE "Heating done."
-	#define MSG_BED_HEATING "Bed Heating."
-	#define MSG_BED_DONE "Bed done."
-	#define MSG_M115_REPORT "FIRMWARE_NAME:Marlin V1; Sprinter/grbl mashup for gen6 FIRMWARE_URL:" FIRMWARE_URL " PROTOCOL_VERSION:" PROTOCOL_VERSION " MACHINE_TYPE:" MACHINE_NAME " EXTRUDER_COUNT:" STRINGIFY(EXTRUDERS) " UUID:" MACHINE_UUID "\n"
-	#define MSG_COUNT_X " Count X: "
-	#define MSG_ERR_KILLED "Printer halted. kill() called!"
-	#define MSG_ERR_STOPPED "Printer stopped due to errors. Fix the error and use M999 to restart. (Temperature is reset. Set it after restarting)"
-	#define MSG_RESEND "Resend: "
-	#define MSG_UNKNOWN_COMMAND "Unknown command: \""
-	#define MSG_ACTIVE_EXTRUDER "Active Extruder: "
-	#define MSG_INVALID_EXTRUDER "Invalid extruder"
-	#define MSG_X_MIN "x_min: "
-	#define MSG_X_MAX "x_max: "
-	#define MSG_Y_MIN "y_min: "
-	#define MSG_Y_MAX "y_max: "
-	#define MSG_Z_MIN "z_min: "
-	#define MSG_Z_MAX "z_max: "
-	#define MSG_M119_REPORT "Reporting endstop status"
-	#define MSG_ENDSTOP_HIT "TRIGGERED"
-	#define MSG_ENDSTOP_OPEN "open"
-	#define MSG_HOTEND_OFFSET "Hotend offsets:"
-
-	#define MSG_SD_CANT_OPEN_SUBDIR "Cannot open subdir"
-	#define MSG_SD_INIT_FAIL "SD init fail"
-	#define MSG_SD_VOL_INIT_FAIL "volume.init failed"
-	#define MSG_SD_OPENROOT_FAIL "openRoot failed"
-	#define MSG_SD_CARD_OK "SD card ok"
-	#define MSG_SD_WORKDIR_FAIL "workDir open failed"
-	#define MSG_SD_OPEN_FILE_FAIL "open failed, File: "
-	#define MSG_SD_FILE_OPENED "File opened: "
-	#define MSG_SD_SIZE " Size: "
-	#define MSG_SD_FILE_SELECTED "File selected"
-	#define MSG_SD_WRITE_TO_FILE "Writing to file: "
-	#define MSG_SD_PRINTING_BYTE "SD printing byte "
-	#define MSG_SD_NOT_PRINTING "Not SD printing"
-	#define MSG_SD_ERR_WRITE_TO_FILE "error writing to file"
-	#define MSG_SD_CANT_ENTER_SUBDIR "Cannot enter subdir: "
-
-	#define MSG_STEPPER_TOO_HIGH "Steprate too high: "
-	#define MSG_ENDSTOPS_HIT "endstops hit: "
-	#define MSG_ERR_COLD_EXTRUDE_STOP " cold extrusion prevented"
-	#define MSG_ERR_LONG_EXTRUDE_STOP " too long extrusion prevented"
-	#define MSG_BABYSTEPPING_X "Babystepping X"
-	#define MSG_BABYSTEPPING_Y "Babystepping Y"
-	#define MSG_BABYSTEPPING_Z "Babystepping Z"
-	#define MSG_SERIAL_ERROR_MENU_STRUCTURE "Error in menu structure"
-#else
+#if !defined(CONSOLE_LANGUAGE_EN)
 	#define MSG_Enqueing "Kolejkowanie \""
 	#define MSG_POWERUP "Zasilanie wlaczone"
 	#define MSG_EXTERNAL_RESET " Reset (zewnetrzny)"
@@ -670,7 +670,7 @@
 	#define MSG_CONTRAST "Contrast"
 
 // Serial Console Messages
-
+#if !defined(CONSOLE_LANGUAGE_EN)
 	#define MSG_Enqueing "Mise en tampon \""
 	#define MSG_POWERUP "Allumage"
 	#define MSG_EXTERNAL_RESET " RAZ Externe"
@@ -744,7 +744,7 @@
 	#define MSG_BABYSTEPPING_Y "Babystepping Y"
 	#define MSG_BABYSTEPPING_Z "Babystepping Z"
 	#define MSG_SERIAL_ERROR_MENU_STRUCTURE "Error in menu structure"
-
+#endif
 #endif
 
 
@@ -874,7 +874,7 @@
 	#define MSG_CONTRAST "Contrast"
 
 // Serial Console Messages
-
+#if !defined(CONSOLE_LANGUAGE_EN)
 	#define MSG_Enqueing "enqueing \""
 	#define MSG_POWERUP "PowerUp"
 	#define MSG_EXTERNAL_RESET " External Reset"
@@ -948,7 +948,7 @@
 	#define MSG_BABYSTEPPING_Y "Babystepping Y"
 	#define MSG_BABYSTEPPING_Z "Babystepping Z"
 	#define MSG_SERIAL_ERROR_MENU_STRUCTURE "Error in menu structure"
-
+#endif
 #endif
 
 
@@ -1083,7 +1083,7 @@
 	#define MSG_CONTRAST "Contrast"
 
 // Serial Console Messages
-
+#if !defined(CONSOLE_LANGUAGE_EN)
 	#define MSG_Enqueing "En cola \""
 	#define MSG_POWERUP "PowerUp"
 	#define MSG_EXTERNAL_RESET " Reset Externo"
@@ -1156,7 +1156,7 @@
 	#define MSG_BABYSTEPPING_Y "Babystepping Y"
 	#define MSG_BABYSTEPPING_Z "Babystepping Z"
 	#define MSG_SERIAL_ERROR_MENU_STRUCTURE "Error in menu structure"
-
+#endif
 #endif
 
 #if LANGUAGE_CHOICE == 6
@@ -1283,7 +1283,7 @@
 	#define MSG_CONTRAST 						"Contrast"
 
 // Serial Console Messages
-
+#if !defined(CONSOLE_LANGUAGE_EN)
 	#define MSG_Enqueing						"Запланировано \""
 	#define MSG_POWERUP							"Включение питания"
 	#define MSG_EXTERNAL_RESET					" Внешний сброс"
@@ -1355,7 +1355,7 @@
 	#define MSG_BABYSTEPPING_Y "Babystepping Y"
 	#define MSG_BABYSTEPPING_Z "Babystepping Z"
 	#define MSG_SERIAL_ERROR_MENU_STRUCTURE "Error in menu structure"
-
+#endif
 #endif
 
 
@@ -1482,7 +1482,7 @@
 	#define MSG_CONTRAST "Contrast"
 
 // Serial Console Messages
-
+#if !defined(CONSOLE_LANGUAGE_EN)
 	#define MSG_Enqueing             "accodamento \""
 	#define MSG_POWERUP              "Accensione"
 	#define MSG_EXTERNAL_RESET       " Reset Esterno"
@@ -1558,7 +1558,7 @@
 	#define MSG_SERIAL_ERROR_MENU_STRUCTURE "Qualcosa non va in MenuStructure."
 
 #endif
-
+#endif
 
 #if LANGUAGE_CHOICE == 8
 
@@ -1689,7 +1689,7 @@
 	#define MSG_CONTRAST "Contrast"
 
 // Serial Console Messages
-
+#if !defined(CONSOLE_LANGUAGE_EN)
 	#define MSG_Enqueing "enqueing \""
 	#define MSG_POWERUP "PowerUp"
 	#define MSG_EXTERNAL_RESET " Reset Externo"
@@ -1763,7 +1763,7 @@
 	#define MSG_BABYSTEPPING_Y "Babystepping Y"
 	#define MSG_BABYSTEPPING_Z "Babystepping Z"
 	#define MSG_SERIAL_ERROR_MENU_STRUCTURE "Algo esta errado na estrutura do Menu."
-
+#endif
 #endif
 
 
@@ -1890,7 +1890,7 @@
 	#define MSG_ENDSTOP_ABORT "Endstop abort"
 
 // Serial Console Messages
-
+#if !defined(CONSOLE_LANGUAGE_EN)
 	#define MSG_Enqueing "jonoon \""
 	#define MSG_POWERUP "Kaynnistys"
 	#define MSG_EXTERNAL_RESET " Ulkoinen Reset"
@@ -1967,6 +1967,7 @@
 	#define MSG_CONTRAST "Kontrasti"
 	#define MSG_SERIAL_ERROR_MENU_STRUCTURE "Virhe valikon rakenteessa"
 
+#endif
 #endif
 
 #if LANGUAGE_CHOICE == 10
@@ -2100,7 +2101,7 @@
 	#define MSG_CONTRAST "Contrast"
 
 // Serial Console Messages
-
+#if !defined(CONSOLE_LANGUAGE_EN)
 	#define MSG_Enqueing "En coda \""
 	#define MSG_POWERUP "PowerUp"
 	#define MSG_EXTERNAL_RESET " Reset Externo"
@@ -2174,6 +2175,7 @@
 	#define MSG_BABYSTEPPING_Z "Babystepping Z"
 	#define MSG_SERIAL_ERROR_MENU_STRUCTURE "Error in menu structure"
 
+#endif
 #endif
 
 #if LANGUAGE_CHOICE == 11 //Dutch
@@ -2298,7 +2300,7 @@
 	#define MSG_ENDSTOP_ABORT "Endstop afbr."
 
 // Serial Console Messages
-
+#if !defined(CONSOLE_LANGUAGE_EN)
 	#define MSG_Enqueing "enqueing \""
 	#define MSG_POWERUP "Opstarten"
 	#define MSG_EXTERNAL_RESET " Externe Reset"
@@ -2373,6 +2375,7 @@
 	#define MSG_BABYSTEPPING_Z "Babystepping Z"
 	#define MSG_SERIAL_ERROR_MENU_STRUCTURE "Fout in menustructuur"
 
+#endif
 #endif
 
 
@@ -2499,7 +2502,7 @@
 	#define MSG_ENDSTOP_ABORT "Endstop abort"
 
 // Serial Console Messages
-
+#if !defined(CONSOLE_LANGUAGE_EN)
 	#define MSG_Enqueing "en cua \""
 	#define MSG_POWERUP "PowerUp"
 	#define MSG_EXTERNAL_RESET " Reset Extern"
@@ -2574,6 +2577,7 @@
 	#define MSG_BABYSTEPPING_Z "Babystepping Z"
 	#define MSG_SERIAL_ERROR_MENU_STRUCTURE "Error a l'estructura dels menus"
 
+#endif
 #endif
 
 //Basque-Euskera
@@ -2699,7 +2703,7 @@
 	#define MSG_ENDSTOP_ABORT "Endstop deuseztat"
 	
 // Serial Console Messages
-
+#if !defined(CONSOLE_LANGUAGE_EN)
 	#define MSG_Enqueing "Zerrendan \""
 	#define MSG_POWERUP "Pizketa"
 	#define MSG_EXTERNAL_RESET " Kanpoko Reset"
@@ -2773,7 +2777,8 @@
 	#define MSG_BABYSTEPPING_Y "Babystepping Y"
 	#define MSG_BABYSTEPPING_Z "Babystepping Z"
 	#define MSG_SERIAL_ERROR_MENU_STRUCTURE "Akatsak menu-an"
-	
+
+#endif	
 #endif
 
 #endif // ifndef LANGUAGE_H
